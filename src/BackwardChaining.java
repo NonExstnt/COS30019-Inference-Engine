0a4d5e7a0cd7429174b8e71dd3a23d68de3e3bab
NonExstnt <64533120+NonExstnt@users.noreply.github.com>
2025-01-28 20:04:06 +1100

    BackwardChaining class creation

---
+++ src/BackwardChaining.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class BackwardChaining {
+    public static boolean check(KnowledgeBase kb, String query) {
+        // Implement the backward chaining algorithm here
+        return false;
+    }
+}
