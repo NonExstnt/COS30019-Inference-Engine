aeecdc07173f4faa1af1a93866fedd25b6a3fa63
NonExstnt <64533120+NonExstnt@users.noreply.github.com>
2025-01-28 20:03:58 +1100

    ForwardChaining class creation

---
+++ src/ForwardChaining.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ForwardChaining {
+    public static boolean check(KnowledgeBase kb, String query) {
+        // Implement the forward chaining algorithm here
+        return false;
+    }
+}
