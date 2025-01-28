1d22cccc565d20b56b1c7626ab4ded1b1d57e56f
NonExstnt <64533120+NonExstnt@users.noreply.github.com>
2025-01-28 20:03:51 +1100

    TruthTable class creation

---
+++ src/TruthTable.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class TruthTable {
+    public static boolean check(KnowledgeBase kb, String query) {
+        // Implement the truth table checking algorithm here
+        return false;
+    }
+}
