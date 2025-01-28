d0ce0a04d6a6585205c7fd7e6348e74559b7fd57
NonExstnt <64533120+NonExstnt@users.noreply.github.com>
2025-01-28 23:42:15 +1100

    Main Comments removed as fixes are brought in

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String[] inputs = new String[2];
-        inputs[0] = "FC";
+        inputs[0] = "BC";
         inputs[1] = "test_HornKB.txt";
 
         InferenceEngine.main(inputs);
